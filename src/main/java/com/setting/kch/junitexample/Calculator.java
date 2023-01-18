d129ae181e3d3b5745b61ad0aa2b3d5304b5c430
KCH <doolysmile1@naver.com>
2023-01-18 13:42:37 +0900

    test : Junit5 공식 문서 참고

---
+++ src/main/java/com/setting/kch/junitexample/Calculator.java
@@ -0,0 +1,7 @@
+package com.setting.kch.junitexample;
+
+public class Calculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/com/setting/kch/junitexample/CalculatorTest.java
@@ -0,0 +1,36 @@
+package com.setting.kch.junitexample;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+class CalculatorTest {
+
+
+
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void addsTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+    }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    void add(int first, int second, int expectedResult) {
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.add(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+
+}
